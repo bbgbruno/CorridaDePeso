f67b3af6ab4f6c52fd0b25327478bb38d465775f
Bruno Borges goes <bbgbruno@gmail.com>
2015-08-21 16:52:27 -0300

    Ajustes no perfil do corredor

--- CorridaDePesso/Models/ManageViewModels.cs
+++ CorridaDePesso/Models/ManageViewModels.cs
@@ -43,18 +43,18 @@ namespace CorridaDePesso.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Senha Atual")]
         public string OldPassword { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nova Senha")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Cofirme a nova senha")]
+        [Compare("NewPassword", ErrorMessage = "A senha digita esta divergente.")]
         public string ConfirmPassword { get; set; }
     }
 
